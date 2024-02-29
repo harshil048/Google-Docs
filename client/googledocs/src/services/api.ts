2562b663583f810f374f1a9b19947c876d717a48
harshil048 <harshilbambhroliya048@gmail.com>
2024-02-29 22:08:09 +0530

    Update BASE_URL in api.ts

--- client/googledocs/src/services/api.ts
+++ client/googledocs/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080/";
+export const BASE_URL = "https://google-docs-weld-theta.vercel.app/";
 
 const API = axios.create({
   baseURL: BASE_URL,
