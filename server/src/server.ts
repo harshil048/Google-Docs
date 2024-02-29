451ec97516b28a11d8620d2fdfd7551db7c07ce8
harshil048 <harshilbambhroliya048@gmail.com>
2024-02-29 22:17:08 +0530

    Remove unnecessary code and update server configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,26 @@
 import http from "http";
-import app from "./index";
 import { Server } from "socket.io";
 import jwt, { VerifyErrors } from "jsonwebtoken";
 import documentService from "./services/document.service";
 import SocketEvent from "./types/enums/socket-events-enum";
+import express, { Express, Request, Response } from "express";
+import db from "./db/models";
+import router from "./routes";
+import errorHandler from "./middleware/errorHandler";
+import cors from "cors";
+
+const app: Express = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://google-docs-frontend-steel.vercel.app",
+    methods: "*",
+  })
+);
+app.use(router);
+app.use(errorHandler);
+
+db.sequelize.sync();
 
 const port = 8080;
 
