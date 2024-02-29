c276def3315176770a08ede45b78ebf84f98f052
harshil048 <harshilbambhroliya048@gmail.com>
2024-02-29 22:20:51 +0530

    Update BASE_URL and allow all origins in CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://google-docs-frontend-steel.vercel.app",
+    origin: "*",
     methods: "*",
   })
 );
