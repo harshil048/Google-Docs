77522f53a978053ba8a8800d00dc286bbe50b741
harshil048 <harshilbambhroliya048@gmail.com>
2024-02-29 18:46:18 +0530

    Remove CORS middleware from index.js

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -6,24 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const models_1 = __importDefault(require("./db/models"));
 const routes_1 = __importDefault(require("./routes"));
-const cors_1 = __importDefault(require("cors"));
 const errorHandler_1 = __importDefault(require("./middleware/errorHandler"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({
-    origin: "https://google-docs-mocha.vercel.app",
-    credentials: true,
-    methods: [
-        "GET",
-        "POST",
-        "PUT",
-        "DELETE",
-        "PATCH",
-        "OPTIONS",
-        "HEAD",
-        "CONNECT",
-    ],
-}));
 app.use(routes_1.default);
 app.use(errorHandler_1.default);
 models_1.default.sequelize.sync();
