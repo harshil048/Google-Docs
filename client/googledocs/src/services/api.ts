5cc50b6cfed1a54597c76c4ba7a69e740c94d171
harshil048 <harshilbambhroliya048@gmail.com>
2024-03-02 11:25:21 +0530

    Update BASE_URL in api.ts

--- client/googledocs/src/services/api.ts
+++ client/googledocs/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://google-docs-backend.vercel.app";
+export const BASE_URL = "http://localhost:8080";
 
 const API = axios.create({
   baseURL: BASE_URL,
